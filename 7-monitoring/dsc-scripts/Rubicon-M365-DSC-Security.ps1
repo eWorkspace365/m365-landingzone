8f3f2fb6dcc5919f44e5665b997316fc98591454
eWorkspace <168641483+eWorkspace365@users.noreply.github.com>
2025-05-02 09:24:46 +0200

    Update Rubicon-M365-DSC-Security.ps1

--- 7-monitoring/dsc-scripts/Rubicon-M365-DSC-Security.ps1
+++ 7-monitoring/dsc-scripts/Rubicon-M365-DSC-Security.ps1
@@ -16,7 +16,7 @@ $choice = Read-Host "Enter 'new' to create a new application or 'update' to upda
 
 if ($choice -eq 'new') {
     # Create AAD Application
-    $AppName =  "Rubicon-M365-DSC-Exchange"
+    $AppName =  "Rubicon-M365-DSC-Security"
     $App = New-MgApplication -DisplayName $AppName 
     $APPObjectID = $App.Id
     Write-Host "Created new application with ID: $APPObjectID"
@@ -31,6 +31,7 @@ if ($choice -eq 'new') {
 
 ###############################################################################
 # Add Permissions
+# URL: https://learn.microsoft.com/en-us/defender-endpoint/api/get-all-recommendations#permissions
 ###############################################################################
 # Combine all required permissions
 $Permissions = @{
@@ -39,28 +40,7 @@ $Permissions = @{
             ResourceAppId = "00000003-0000-0000-c000-000000000000"
             ResourceAccess = @(
                 @{
-                    id = "7ab1d382-f21e-4acd-a863-ba3e13f7da61"  # Directory.Read.All
-                    type = "Role"
-                },
-                @{
-                    id = "810c84a8-4a9e-49e6-bf7d-12d183f40d01"  # Mail.Read
-                    type = "Role"
-                },
-		@{
-                    id = "b633e1c5-b582-4048-a93e-9f11b44c7e96"  # Mail.Send
-                    type = "Role"
-                },
-                @{
-                    id = "9e3f62cf-ca93-4989-b6ce-bf83c28f9fe8"  # RoleManagement.ReadWrite.Directory
-                    type = "Role"
-                }
-            )
-        },
-        @{
-            resourceAppId = "00000002-0000-0ff1-ce00-000000000000"  # Office 365 Exchange Online
-            resourceAccess = @(
-                @{
-                    id = "dc50a0fb-09a3-484d-be87-e023b12c6440"  # Exchange.ManageAsApp
+                    id = "0000000-0000-0000-0000-00000000000"  # SecurityRecommendation.Read.All > Graph API = WindowsDefenderATP
                     type = "Role"
                 }
             )
